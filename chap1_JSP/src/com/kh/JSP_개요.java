07cca8e549139e41404f67277eb9bce01b9e3291
오재진 <130115836+renewjin@users.noreply.github.com>
2024-09-09 10:50:15 +0900

    jsp 개요

---
+++ chap1_JSP/src/com/kh/JSP_개요.java
@@ -0,0 +1,14 @@
+package com.kh;
+/*
+ * JSP : Java Server Pages
+ * Html 코드 안에서 자바 코드를 작성할 수 있음
+ * 모든 Java 코드 까지 html 작성하는 페이지 안에서 작성하는 것이 아니라
+ * 부분적으로 필요한 기능만 Java로 작성
+ * JSP와 같은 프로젝트를 진행하기 위해서는
+ * ThymeLeaf 와 같은 환경으로 사용
+ * Dynamic Web Project vs Springboot 환경으로 실행
+ * 중간과제 타임리프 -> JSP 작성
+ * */
+public class JSP_개요 {
+
+}
